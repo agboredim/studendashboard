a979a4b8127e5e63f93fe07e2da19cb4c40477e7
Abayomi Aishida <yomicorperate@gmail.com>
2025-05-02 09:18:17 +0100

    fix: replace spinner image and update import path in Spinner component

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,9 +1,9 @@
-import bgImage from "@/assets/img/spin.png";
+import spin from "@/assets/img/spinner.png";
 
 function Spinner() {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center backdrop-blur bg-opacity-50 z-50">
-      <img src={bgImage} alt="Loading spinner" className="animate-spin w-60" />
+      <img src={spin} alt="Loading spinner" className="animate-spin w-60" />
     </div>
   );
 }
